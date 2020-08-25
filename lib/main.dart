d4e5b197880c9b52fe5361de27390d88a3531d58
cerveau08 <malickcoly342@gmail.com>
2020-08-25 13:30:47 +0000

    calorie calcule ok

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  
+  int calorieBase;
+  int calorieAvecActivite;
+  int radioSelectionnee;
   double poids;
+  double age;
+  bool genre = false;
+  double taille = 170.0;
+
+  Map mapActivite = {
+    0: "Faible",
+    1: "Modere",
+    2: "Forte"
+  }; 
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -38,8 +51,10 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        backgroundColor: setColor(),
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
@@ -48,6 +63,47 @@ class _MyHomePageState extends State<MyHomePage> {
               elevation: 10.0,
               child: Column(
                 children: [
+                  padding(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      textAvecStyle("Femme", color: Colors.pink),
+                      Switch(
+                        value: genre,
+                        inactiveTrackColor: Colors.pink,
+                        activeColor: Colors.blue, 
+                        onChanged: (bool b) {
+                          setState(() {
+                            genre = b;
+                          });
+                        },
+                      ),
+                      textAvecStyle("Homme", color: Colors.blue)
+                    ],
+                  ),
+                  padding(),
+                  RaisedButton(
+                    color: setColor(),
+                    child: textAvecStyle((age == null)? "Appuyer pour entrer votre age": "Votre age est de : ${age.toInt()}",
+                      color: Colors.white
+                    ),
+                    onPressed: (() => montrerPicker()),
+                  ),
+                  padding(),
+                  textAvecStyle("Votre taille est de ${taille.toInt()} cm.", color: setColor()),
+                  padding(),
+                  Slider(
+                    value: taille,
+                    activeColor: setColor(),
+                    onChanged: (double d) {
+                      setState(() {
+                        taille = d;
+                      });
+                    },
+                    max: 230.0,
+                    min: 100.0,
+                  ),
+                  padding(),
                   TextField(
                     keyboardType: TextInputType.number,
                     onChanged: (String string) {
@@ -56,11 +112,20 @@ class _MyHomePageState extends State<MyHomePage> {
                       });
                     },
                     decoration: InputDecoration(
-                      labelText: "Entrez votre poids en kilo"
+                      labelText: "Entrez votre poids en kilo",
                     ),
-                  )
+                  ),
+                  padding(),
+                  textAvecStyle("Quelle est votre activite sportive?", color: setColor()),
+                  rowRadio(),
+                  padding(),
                 ],
               ),
+            ),
+            RaisedButton(
+              color: setColor(),
+              child: textAvecStyle("Calculer", color: Colors.white),
+              onPressed: calculerNombredeCalories,
             )
           ]
         ),
@@ -68,6 +133,36 @@ class _MyHomePageState extends State<MyHomePage> {
     )
     ); 
     
+  }
+  
+  Padding padding() {
+    return Padding(padding: EdgeInsets.only(top: 15.0),);
+  }
+
+  Future<Null> montrerPicker() async {
+    DateTime choix = await showDatePicker(
+      context: context, 
+      initialDate: DateTime.now(), 
+      firstDate: DateTime(1900), 
+      lastDate: DateTime.now(),
+      initialDatePickerMode: DatePickerMode.year
+    );
+    if (choix != null) {
+      var difference = DateTime.now().difference(choix);
+      var jours = difference.inDays;
+      var ans = (jours / 365);
+      setState(() {
+        age = ans;
+      });
+    }
+  }
+
+  Color setColor() {
+    if (genre) {
+      return Colors.blue;
+    } else {
+      return Colors.pink;
+    }
   }
 
   Text textAvecStyle(String data, {color: Colors.black, fontSize: 15.0}) {
@@ -78,6 +173,109 @@ class _MyHomePageState extends State<MyHomePage> {
         color: color,
         fontSize: fontSize
       ),
+    );
+  }
+
+  Row rowRadio() {
+    List<Widget> l = [];
+    mapActivite.forEach((key, value) {
+      Column colonne = Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Radio(
+            activeColor: setColor(),
+            value: key,
+            groupValue: radioSelectionnee,  
+            onChanged: (Object i) {
+              setState(() {
+                radioSelectionnee = i;
+              });
+            },
+          ),
+          textAvecStyle(value, color: setColor())
+        ],
+      );
+      l.add(colonne);
+    });
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: l,
+    );
+  }
+
+  void calculerNombredeCalories(){
+    if (age != null && poids != null && radioSelectionnee != null) {
+      if (genre) {
+        calorieBase = (66.4730 + (13.7516 * poids) + (5.0033 * taille) - (6.7550 * age)).toInt();
+      } else {
+        calorieBase = (655.0955 + (9.5634 * poids) + (1.8496 * taille) - (4.6756 * age)).toInt();
+      }
+      switch(radioSelectionnee) {
+        case 0:
+          calorieAvecActivite = (calorieBase * 1.2).toInt();
+          break;
+        case 1:
+          calorieAvecActivite = (calorieBase * 1.5).toInt();
+          break;
+        case 2:
+          calorieAvecActivite = (calorieBase * 1.8).toInt();
+          break;
+        default:
+          calorieAvecActivite = calorieBase;
+      }
+      setState(() {
+        dialogue();
+      });
+    } else {
+      alerte(); 
+    }
+  }
+  
+  Future<Null> dialogue() async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext buildContext) {
+        return SimpleDialog(
+          title: textAvecStyle("Votre besoin en calories", color: setColor()),
+          contentPadding: EdgeInsets.all(15.0),
+          children: [
+            padding(),
+            textAvecStyle("Votre besoin de base est de :$calorieBase"),
+            padding(),
+            textAvecStyle("Votre besoin avec activite sportive est de : $calorieAvecActivite"),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pop(buildContext);
+              },
+              child: textAvecStyle("Ok", color: Colors.white),
+              color: setColor(),
+            )
+          ],
+        );
+        
+      }
+    );
+  }
+
+  Future<Null> alerte() async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext buildContext) {
+        return AlertDialog(
+          title: textAvecStyle("Erreur"),
+          content: textAvecStyle("Tous les champs ne sont pas remplis"),
+          actions: [
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(buildContext);
+              },
+              child: textAvecStyle("Ok", color: Colors.red),
+            )
+          ],
+        );
+      }
     );
   }
 }
